a1866be42699b725a9931d2a057bf111ce110582
João M. Gomes <joaomarquesgomes94@gmail.com>
2024-08-26 16:10:15 +0100

    cors added

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+    app.use(cors());
     app.use(bodyParser.json());
+
+
     const properties = [];
 
 //Endpoint to add a new property
